9ebf4dec34a4047d40f213730965c91bae911d9e
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 14:45:38 +0530

    inital commit

---
+++ src/main/java/com/medisphere/apigateway/MedisphereApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedisphereApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedisphereApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/apigateway/MedisphereApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisphereApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
