3469d2f1f699ebab9b914abb0970bc99998d1e49
AroshXR <aroshanasandeep454@gmail.com>
2026-04-05 22:06:12 +0530

    application.properties

--- src/main/java/com/medisphere/apigateway/MedisphereApiGatewayApplication.java
+++ src/main/java/com/medisphere/apigateway/MedisphereApiGatewayApplication.java
@@ -1,9 +1,10 @@
 package com.medisphere.apigateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedisphereApiGatewayApplication {
 
 	public static void main(String[] args) {
