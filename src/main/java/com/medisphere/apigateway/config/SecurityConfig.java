b78de4ee7de7b49c728bd1988a2e391638d27aac
AroshXR <aroshanasandeep454@gmail.com>
2026-04-16 13:54:28 +0530

    some mdifications

--- src/main/java/com/medisphere/apigateway/config/SecurityConfig.java
+++ src/main/java/com/medisphere/apigateway/config/SecurityConfig.java
@@ -14,6 +14,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll());
         return http.build();
